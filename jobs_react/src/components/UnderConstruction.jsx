808b7384ef56ad48c4c73963c0cb50467d3e2041
Spirit1269 <kimberlydjenkins1@gmail.com>
2025-04-23 15:41:26 -0500

    Update page with email.

--- jobs_react/src/components/UnderConstruction.jsx
+++ jobs_react/src/components/UnderConstruction.jsx
@@ -2,11 +2,8 @@ export default function UnderConstruction() {
     return (
       <div style={{ textAlign: 'center', padding: '100px' }}>
         <h1>🚧 TriPowers LLC</h1>
-        <p>We're working on something great. Check back soon!</p>
-        
-        <div className="bg-purple-600 text-white p-4 rounded-md">
-            Tailwind is working!
-        </div>
+        <p>We're working on something great. Check back soon!</p>       
+        <p>In the meantime, feel free to reach out to us at <a href="mailto:kimberlyjenkins@tripowersllc.com"/></p>
       </div>
     );
   }
