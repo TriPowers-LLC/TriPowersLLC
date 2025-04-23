f7b038d2147f522ae34aa4d1e05b02c137056663
Spirit1269 <kimberlydjenkins1@gmail.com>
2025-04-23 15:51:18 -0500

    Update page.

--- jobs_react/src/components/UnderConstruction.jsx
+++ jobs_react/src/components/UnderConstruction.jsx
@@ -3,7 +3,12 @@ export default function UnderConstruction() {
       <div style={{ textAlign: 'center', padding: '100px' }}>
         <h1>🚧 TriPowers LLC</h1>
         <p>We're working on something great. Check back soon!</p>       
-        <p>In the meantime, feel free to reach out to us at <a href="mailto:kimberlyjenkins@tripowersllc.com"/></p>
+        <p className="text-sm text-gray-600">
+          In the meantime, feel free to reach out to us at&nbsp;   
+           <a href="mailto:kimberlyjenkins@tripowersllc.com"> 
+             kimberlyjenkins@tripowersllc.com
+          </a>
+        </p>      
       </div>
     );
   }
