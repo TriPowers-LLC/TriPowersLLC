f80aa36d23e483f6c0445d501bb6817ed3817367
Spirit1269 <kimberlydjenkins1@gmail.com>
2025-05-22 19:26:07 -0500

    Update App.jsx and index.css

--- jobs_react/src/App.jsx
+++ jobs_react/src/App.jsx
@@ -2,32 +2,27 @@ import React from 'react';
 import { store } from './actions/store';
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import './styles/App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import UnderConstruction from './components/UnderConstruction';
-import logo from './assets/logo.jpg'; // Import your logo image
-function App() {
+import Services from './components/Services';
+import Contact from './components/contact';
+
+const App = ()=> {
   return (
     <> 
       <Provider store={store}>
-        <Router>
-          {/* <NavBar /> */}
+        <Router>       
+          <NavBar />
           <Routes>
-            {/* <Route path="/" element={<Home />} />  */}
-            <Route path="/" element={<UnderConstruction />} />
+            <Route path="/" element={<Home />} /> 
+            <Route path="/contact" element={<Contact />} />
+            {/* <Route path="/" element={<UnderConstruction />} /> */}
           </Routes>
-          <div className="flex justify-center items-center min-h-screen bg-white">
-          <img
-            src={logo}
-            alt="Logo"
-            className="App-logo"
-          />
-        </div>
+          <Services/>
           
         </Router>
-        
+       
       </Provider>
     </>
   );
