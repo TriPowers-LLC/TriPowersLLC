c20e3013e1129dd3209657beffbef7fca8fdac31
Kimberly Jenkins <116847130+Spirit1269@users.noreply.github.com>
2026-03-20 13:15:38 -0500

    Fix login page auth store wiring

--- jobs_react/src/actions/store.js
+++ jobs_react/src/actions/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import applicantsReducer, { fetchApplicants } from '../slices/applicantsSlice.js';
 import jobsSlice from '../slices/jobsSlice.js';
 import applicationsSlice from '../slices/applicationsSlice.js';
+import authReducer from '../slices/authSlice.js';
 
 export const store = configureStore({
   reducer: {
     applicants: applicantsReducer,
     jobs: jobsSlice.reducer,
     applications: applicationsSlice.reducer,
+    auth: authReducer,
   },
 });
 
